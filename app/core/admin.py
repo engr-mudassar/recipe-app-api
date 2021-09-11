8c1ba0322f7d80169f6734a567f7c3ffb3b43726
Mudassar Saeed <Mudassar.Saeed@ibm.com>
2021-09-11 16:33:10 +0500

    DB setup completed

--- app/core/admin.py
+++ app/core/admin.py
@@ -15,7 +15,7 @@ class UserAdmin(BaseUserAdmin):
             _('Permissions'),
             {'fields': ('is_active', 'is_staff', 'is_superuser')}
         ),
-        (_('Important dates'), {'fields': ('last_login',)})
+        (_('Important Dates'), {'fields': ('last_login',)})
     )
     add_fieldsets = (
         (None, {
@@ -23,6 +23,6 @@ class UserAdmin(BaseUserAdmin):
             'fields': ('email', 'password1', 'password2')
         }),
     )
-    
+
 
 admin.site.register(models.User, UserAdmin)
